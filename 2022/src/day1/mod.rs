5f5c2e55363c08e928ca2241bab62ed5fc0e0151
Tyderion <nadlergabriel@gmail.com>
2022-12-07 12:54:14 +0100

    More cleanup, add simple test for example case

--- 2022/src/day1/mod.rs
+++ 2022/src/day1/mod.rs
@@ -2,10 +2,8 @@ use std::cmp::Reverse;
 
 use crate::fileutils;
 
-pub fn run() {
-    let filename = "./src/day1/input.txt";
-    let lines = fileutils::lines_from_file(filename);
-    let mut grouped_calories = lines.unwrap().into_iter().map(|l| l.unwrap()).fold(
+fn compute_calories(lines: Vec<String>) -> Vec<i32> {
+    let mut grouped_calories = lines.iter().fold(
         vec![0],
         |mut result, current| {
             match current {
@@ -16,9 +14,28 @@ pub fn run() {
         },
     );
     grouped_calories.sort_by_key(|n| Reverse(*n));
+    grouped_calories
+}
 
+pub fn run() {
+    let filename = "./src/day1/input.txt";
+    let lines = fileutils::lines_from_file(filename);
+
+    let strings = lines.unwrap().into_iter().map(|l| l.unwrap()).collect();
+    let grouped_calories = compute_calories(strings);
     println!("Max: {:?}", grouped_calories.iter().max().unwrap());
 
     let top3 = grouped_calories.iter().take(3).sum::<i32>();
     println!("Top 3 carry: {:?}", top3);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn calorie_are_computed_and_ordered_correctly() {
+        let result = compute_calories(["1000","2000","3000","","4000","","5000","6000","","7000","8000","9000","","10000"].map(String::from).to_vec());
+        assert_eq!(result, vec![24000, 11000, 10000, 6000, 4000]);
+    }
 }
