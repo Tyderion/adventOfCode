efbfac7bbf17b0b0ac732e79db5894d7f08c6e1e
Tyderion <nadlergabriel@gmail.com>
2022-12-16 22:43:36 +0100

    initial day11 setup

---
+++ 2022/day_11/src/main.rs
@@ -0,0 +1,45 @@
+fn main() {
+    let filename = "day_10/src/input.txt";
+    let lines = fileutils::lines_from_file(filename);
+
+    let part1_result = part1(lines.clone());
+    println!("Monkey Business Level: {}", part1_result);
+
+    let part2_result = part2(lines.clone());
+    println!("Part2: ??{}", part2_result);
+}
+
+fn part1<T: AsRef<str>>(lines: Vec<T>) -> i32 {
+    todo!()
+}
+
+fn part2<T: AsRef<str>>(lines: Vec<T>) -> i32 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn example_input() -> Vec<String> {
+        let filename = "src/example.txt";
+        match fileutils::safe_lines_from_file(filename) {
+            Some(lines) => lines,
+            // When debugging we start in root, else in day_11
+            _ => fileutils::lines_from_file("day_11/".to_string() + filename),
+        }
+    }
+
+    #[test]
+    fn example_case_part1() {
+        let result = part1(example_input());
+        assert_eq!(result, 10605);
+    }
+
+    #[test]
+    fn example_case_part2() {
+        // Cannot be tested as it draws letters on the command line
+        let result = part2(example_input());
+        assert_eq!(result, 2);
+    }
+}
