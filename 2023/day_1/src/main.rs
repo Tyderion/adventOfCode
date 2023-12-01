d19e1c186eef7df6004fa03cf527da178118796a
Gabriel Nadler <gabriel.nadler@digitecgalaxus.ch>
2023-12-01 10:51:05 +0100

    part 1

--- 2023/day_1/src/main.rs
+++ 2023/day_1/src/main.rs
@@ -13,8 +13,21 @@ pub fn main() {
     println!("Sum of nubmers: {}", part2_result);
 }
 
-fn part1(_lines: &Vec<String>) -> u32 {
-    return 0
+fn part1(lines: &Vec<String>) -> u32 {
+    return lines
+        .iter()
+        .map(|l| {
+            l.chars()
+                .into_iter()
+                .filter(|c| match c {
+                    '0'..='9' => true,
+                    _ => false,
+                })
+                .map(|c| c.to_digit(10).unwrap())
+                .collect::<Vec<_>>()
+        })
+        .map(|nums| nums.first().unwrap() * 10 + nums.last().unwrap())
+        .sum::<u32>();
 }
 
 fn part2(_lines: &Vec<String>) -> u32 {
