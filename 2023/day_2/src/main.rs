cf8afe38dd1c0a36f9012ab903e8f9bd9c3c53ef
Gabriel Nadler <gabriel.nadler@digitecgalaxus.ch>
2023-12-02 13:28:21 +0100

    Part 1

--- 2023/day_2/src/main.rs
+++ 2023/day_2/src/main.rs
@@ -13,8 +13,114 @@ pub fn main() {
     // println!("Sum of part 2: {}", part2_result);
 }
 
-fn part1(_lines: &Vec<String>) -> u32 {
-    return 0;
+#[derive(Debug)]
+struct Pull {
+    red: u32,
+    green: u32,
+    blue: u32,
+}
+
+impl Pull {
+    pub fn default() -> Pull {
+        return Pull {
+            red: 0,
+            green: 0,
+            blue: 0,
+        };
+    }
+
+    fn red(&self, red: u32) -> Pull {
+        Pull {
+            red: red,
+            green: self.green,
+            blue: self.blue,
+        }
+    }
+    fn green(&self, green: u32) -> Pull {
+        Pull {
+            red: self.red,
+            green: green,
+            blue: self.blue,
+        }
+    }
+    fn blue(&self, blue: u32) -> Pull {
+        Pull {
+            red: self.red,
+            green: self.green,
+            blue: blue,
+        }
+    }
+
+    /*  1 red, 2 green, 6 blue */
+    pub fn from(str: impl AsRef<str>) -> Pull {
+        return str
+            .as_ref()
+            .trim()
+            .split(",")
+            .fold(Pull::default(), |pull, single_color| {
+                match single_color.trim().split(" ").collect::<Vec<_>>()[..] {
+                    [num, "red"] => pull.red(num.parse::<u32>().unwrap()),
+                    [num, "green"] => pull.green(num.parse::<u32>().unwrap()),
+                    [num, "blue"] => pull.blue(num.parse::<u32>().unwrap()),
+                    _ => panic!("There should be no other option"),
+                }
+            });
+    }
+
+    pub fn is_valid(&self, red: u32, green: u32, blue: u32) -> bool {
+        return self.red <= red && self.green <= green && self.blue <= blue;
+    }
+}
+
+#[derive(Debug)]
+struct Game {
+    id: u32,
+    pulls: Vec<Pull>,
+}
+
+fn parse_game_id(str: impl AsRef<str>) -> Option<u32> {
+    return match str.as_ref().split(" ").collect::<Vec<_>>()[..] {
+        [_, num] => num.parse::<u32>().ok(),
+        _ => None,
+    };
+}
+
+fn parse_pulls(str: impl AsRef<str>) -> Vec<Pull> {
+    return str.as_ref().split(";").map(Pull::from).collect::<Vec<_>>();
+}
+
+impl Game {
+    pub fn from(str: impl AsRef<str>) -> Option<Game> {
+        return match str.as_ref().split(":").collect::<Vec<_>>()[..] {
+            [game, data] => match parse_game_id(game) {
+                Some(id) => Some(Game {
+                    id,
+                    pulls: parse_pulls(data),
+                }),
+                None => None,
+            },
+            _ => None,
+        };
+    }
+}
+
+fn get_games(lines: &Vec<String>) -> Vec<Game> {
+    return lines.iter().map(Game::from).filter_map(|v| v).collect();
+}
+
+fn part1(lines: &Vec<String>) -> u32 {
+    const RED_LIMIT: u32 = 12;
+    const GREEN_LIMIT: u32 = 13;
+    const BLUE_LIMIT: u32 = 14;
+    return get_games(lines)
+        .iter()
+        .filter(|g| {
+            g.pulls
+                .iter()
+                .all(|p| p.is_valid(RED_LIMIT, GREEN_LIMIT, BLUE_LIMIT))
+        })
+        .map(|g| g.id)
+        .sum();
 }
 
 fn part2(_lines: &Vec<String>) -> u32 {
