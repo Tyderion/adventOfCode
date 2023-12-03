9706e61bd876c2bd0f7aec29ded41ceccfeaa142
Gabriel Nadler <gabriel.nadler@digitecgalaxus.ch>
2023-12-03 14:26:20 +0100

    Don't pad lines

--- 2023/day_3/src/engine.rs
+++ 2023/day_3/src/engine.rs
@@ -20,37 +20,32 @@ pub struct Engine {
     pub part_numbers: HashMap<usize, Vec<PartNumber>>,
 }
 
+fn add_part_number(number: &Option<(usize, usize, String)>, list: &mut Vec<PartNumber>) {
+    if let Some((start, end, num)) = &number {
+        list.push(PartNumber {
+            id: num.parse::<u32>().unwrap(),
+            start: *start,
+            end: *end,
+        })
+    }
+}
+
 impl Engine {
     pub fn parse(input: &Vec<impl AsRef<str>>) -> Engine {
         let mut parts = vec![];
         let mut part_numbers = HashMap::new();
 
         input.iter().enumerate().for_each(|(row, l)| {
-            let mut parts_nums: Vec<PartNumber> = vec![];
+            let mut part_nums: Vec<PartNumber> = vec![];
             let mut number: Option<(usize, usize, String)> = None;
-            let mut padded_line = l.as_ref().to_string();
-            padded_line.push('.');
-            padded_line.char_indices().for_each(|(col, c)| match c {
+            l.as_ref().char_indices().for_each(|(col, c)| match c {
                 '0'..='9' => {
                     number = match &number {
-                        Some((ref start, _, ref num)) => {
-                            let mut new_value = String::from(num);
-                            new_value.push(c);
-                            Some((*start, col, new_value.to_string()))
-                        }
+                        Some((start, _, num)) => Some((*start, col, format!("{}{}", num, c))),
                         None => Some((col, col, String::from(c))),
                     }
                 }
                 _ => {
-                    match &number {
-                        None => (),
-                        Some((start, end, num)) => parts_nums.push(PartNumber {
-                            id: num.parse::<u32>().unwrap(),
-
-                            start: *start,
-                            end: *end,
-                        }),
-                    }
                     match c {
                         'A'..='z' | '.' => (),
                         _ => parts.push(Part {
@@ -59,11 +54,13 @@ impl Engine {
                             col,
                         }),
                     }
+                    add_part_number(&number, &mut part_nums);
                     number = None;
                 }
             });
-            if parts_nums.len() > 0 {
-                part_numbers.insert(row, parts_nums);
+            add_part_number(&number, &mut part_nums);
+            if part_nums.len() > 0 {
+                part_numbers.insert(row, part_nums);
             }
         });
 
