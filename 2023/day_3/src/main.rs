329fed484fa3f3a83c45eedc4cbef8d9255fd2af
Gabriel Nadler <gabriel.nadler@digitecgalaxus.ch>
2023-12-03 10:43:07 +0100

    day 3 setup

---
+++ 2023/day_3/src/main.rs
@@ -0,0 +1,46 @@
+pub fn main() {
+    let filename = "day_3/src/input.txt";
+    let input = fileutils::safe_lines_from_file(filename);
+    let part1_result = match input {
+        None => panic!("No input received"),
+        Some(ref lines) => part1(lines),
+    };
+    let part2_result = match input {
+        None => panic!("No input received"),
+        Some(ref lines) => part2(lines),
+    };
+    println!("Sum of games: {}", part1_result);
+    println!("Sum of part 2: {}", part2_result);
+}
+
+fn part1(_lines: &Vec<String>) -> u32 {
+    return 0;
+}
+
+fn part2(_lines: &Vec<String>) -> u32 {
+    return 0;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_INPUT1: [&str; 10] = [
+        "467..114..",
+        "...*......",
+        "..35..633.",
+        "......#...",
+        "617*......",
+        ".....+.58.",
+        "..592.....",
+        "......755.",
+        "...$.*....",
+        ".664.598..",
+    ];
+
+    #[test]
+    fn example_case_part1() {
+        let result = part1(&EXAMPLE_INPUT1.iter().map(|x| String::from(*x)).collect());
+        assert_eq!(result, 4361);
+    }
+}
