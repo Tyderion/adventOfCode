c60e4dee259aa96f156d15c63ab7c5d329c75872
Gabriel Nadler <gabriel.nadler@digitecgalaxus.ch>
2023-12-04 09:21:04 +0100

    day4 - setup

---
+++ 2023/day_4/src/main.rs
@@ -0,0 +1,42 @@
+pub fn main() {
+    let filename = "day_4/src/input.txt";
+    let input = fileutils::safe_lines_from_file(filename);
+    let part1_result = match input {
+        None => panic!("No input received"),
+        Some(ref lines) => part1(lines),
+    };
+    let part2_result = match input {
+        None => panic!("No input received"),
+        Some(ref lines) => part2(lines),
+    };
+    println!("Sum of games: {}", part1_result);
+    println!("Sum of part 2: {}", part2_result);
+}
+
+fn part1(_lines: &Vec<impl AsRef<str>>) -> u32 {
+    0
+}
+
+fn part2(_lines: &Vec<impl AsRef<str>>) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_INPUT1: [&str; 6] = [
+        "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+        "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+        "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+        "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+        "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+        "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+    ];
+
+    #[test]
+    fn example_case_part1() {
+        let result = part1(&EXAMPLE_INPUT1.iter().map(|x| String::from(*x)).collect());
+        assert_eq!(result, 13);
+    }
+}
