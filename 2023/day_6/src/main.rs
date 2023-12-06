702201dadfa5892dc860fca831da697be5f62cc8
Gabriel Nadler <gabriel.nadler@digitecgalaxus.ch>
2023-12-06 11:37:21 +0100

    Part 1

--- 2023/day_6/src/main.rs
+++ 2023/day_6/src/main.rs
@@ -13,8 +13,19 @@ pub fn main() {
     println!("Sum of part 2: {}", part2_result);
 }
 
-fn part1(_lines: &Vec<impl AsRef<str>>) -> u32 {
-    0
+fn parse_p1(lines: &Vec<impl AsRef<str>>) -> Vec<(u32, u32)> {
+    let times = lines[0].as_ref().split(" ").filter_map(|t| t.parse::<u32>().ok());
+    let distances = lines[1].as_ref().split(" ").filter_map(|t| t.parse::<u32>().ok());
+    times.zip(distances).collect()
+}
+
+fn part1(lines: &Vec<impl AsRef<str>>) -> usize {
+    let races = parse_p1(lines);
+    println!("{:?}", races);
+    races.iter().map(|(time, distance)| {
+        (1..*time).map(|t| t * (time -t)).filter(|d| d > distance).collect::<Vec<_>>().len()
+    }).product()
+}
 }
 
 fn part2(_lines: &Vec<impl AsRef<str>>) -> u32 {
