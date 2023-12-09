d62d653777164681c7d6ed6c3e377bd362f438a2
Gabriel Nadler <gabriel.nadler@digitecgalaxus.ch>
2023-12-09 12:30:11 +0100

    Initial Setup Day 8

---
+++ 2023/day_8/src/main.rs
@@ -0,0 +1,59 @@
+pub fn main() {
+    let filename = "day_8/src/input.txt";
+    let input = fileutils::safe_lines_from_file(filename);
+    let part1_result = match input {
+        None => panic!("No input received"),
+        Some(ref lines) => part1(lines),
+    };
+    let part2_result = match input {
+        None => panic!("No input received"),
+        Some(ref lines) => part2(lines),
+    };
+    println!("Sum of games: {}", part1_result);
+    println!("Sum of part 2: {}", part2_result);
+}
+
+fn part1(_lines: &Vec<impl AsRef<str>>) -> u64 {
+    0
+}
+
+fn part2(_lines: &Vec<impl AsRef<str>>) -> u64 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_INPUT1: [&str; 9] = [
+        "RL",
+        "",
+        "AAA = (BBB, CCC)",
+        "BBB = (DDD, EEE)",
+        "CCC = (ZZZ, GGG)",
+        "DDD = (DDD, DDD)",
+        "EEE = (EEE, EEE)",
+        "GGG = (GGG, GGG)",
+        "ZZZ = (ZZZ, ZZZ)",
+    ];
+
+    const EXAMPLE_INPUT2: [&str; 5] = [
+        "LLR",
+        "",
+        "AAA = (BBB, BBB)",
+        "BBB = (AAA, ZZZ)",
+        "ZZZ = (ZZZ, ZZZ)",
+    ];
+
+    #[test]
+    fn example_case1_part1() {
+        let result = part1(&EXAMPLE_INPUT1.iter().map(|x| String::from(*x)).collect());
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn example_case2_part1() {
+        let result = part1(&EXAMPLE_INPUT2.iter().map(|x| String::from(*x)).collect());
+        assert_eq!(result, 6);
+    }
+}
